120db687b1e925043a6122927e8dde6ae709a07d
Bartlomiej Pasik <bartlomiej.pasik@espeo.eu>
2019-11-04 14:25:39 +0100

    Separate App from Server

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
