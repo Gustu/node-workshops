28ec8bef83e894695e1e0a9d15ed9fa873c03de4
Bartlomiej Pasik <bartlomiej.pasik@espeo.eu>
2019-11-05 15:14:49 +0100

    Added Not Found route handler. Added correct status to default error handler.

--- app.js
+++ app.js
@@ -13,9 +13,13 @@ app.use(bodyParser.json());
 
 app.use('/api', ApiRoot({ tweetConfig: config }));
 
+app.use((req, res, next) => {
+  res.status(404).send(`Not found ${req.path}`);
+});
+
 app.use((err, req, res, next) => {
   console.log(err);
-  res.send(`Something gone wrong. ${err.message}`);
+  res.status(500).send(`Something gone wrong. ${err.message}`);
 });
 
 module.exports = app;
