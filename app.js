8ea090f616e00ee1b69bdea3bba5c8ad129ea827
Bartlomiej Pasik <bartlomiej.pasik@espeo.eu>
2019-11-05 13:44:57 +0100

    Async errors and default error handler

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const ApiRoot = require('./controllers');
 const TweetConfig = require('./config/TweetConfig');
 
@@ -6,6 +7,14 @@ const config = TweetConfig({});
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/api', ApiRoot({ tweetConfig: config }));
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.send(`Something gone wrong. ${err.message}`);
+});
 
 module.exports = app;
