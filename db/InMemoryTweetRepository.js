a9716cc5c24bfa2b102e4f1dcb1b4c1d95d271fa
Bartlomiej Pasik <bartlomiej.pasik@espeo.eu>
2019-11-06 12:06:13 +0100

    Move validation logic to repository.
    Those constraints will be checked at the db level, so that's why they are moved to InMemoryRepository.

--- db/InMemoryTweetRepository.js
+++ db/InMemoryTweetRepository.js
@@ -1,4 +1,9 @@
+const validator = require('validator');
 const Tweet = require('../models/Tweet');
+const Uuid = require('../common/Uuid');
+
+const MESSAGE_MIN_CHARS = 1;
+const MESSAGE_MAX_CHARS = 255;
 
 const InMemoryTweetRepository = (seed) => {
   const tweets = seed || {};
@@ -7,6 +12,14 @@ const InMemoryTweetRepository = (seed) => {
 
   return {
     async createNew(tweet) {
+      if (!validator.isLength(tweet.message, { min: MESSAGE_MIN_CHARS, max: MESSAGE_MAX_CHARS })) {
+        throw new TypeError(`Twitter does not match allowed range. Length ${tweet.message.length} of [${MESSAGE_MIN_CHARS}, ${MESSAGE_MAX_CHARS}]`);
+      }
+
+      if (!Uuid.isValid(tweet.writerId)) {
+        throw new TypeError(`Writer id needs to be valid UUID ${tweet.writerId}`);
+      }
+
       tweets[tweet.tweetId] = tweet;
       return tweet;
     },
--- models/Tweet.js
+++ models/Tweet.js
@@ -1,10 +1,7 @@
 const { differenceInSeconds } = require('date-fns');
-const validator = require('validator');
 const Uuid = require('../common/Uuid');
 
 const MAX_MINUTES_FOR_DELETION_IN_SEC = 3 * 60;
-const MESSAGE_MIN_CHARS = 1;
-const MESSAGE_MAX_CHARS = 255;
 
 const Tweet = ({
   tweetId = Uuid.generate(),
@@ -12,22 +9,12 @@ const Tweet = ({
   message,
   clock,
   createdAt = clock(),
-}) => {
-  if (!validator.isLength(message, { min: MESSAGE_MIN_CHARS, max: MESSAGE_MAX_CHARS })) {
-    throw new TypeError(`Twitter does not match allowed range. Length ${message.length} of [${MESSAGE_MIN_CHARS}, ${MESSAGE_MAX_CHARS}]`);
-  }
-
-  if (!Uuid.isValid(writerId)) {
-    throw new TypeError(`Writer id needs to be valid UUID ${writerId}`);
-  }
-
-  return {
-    tweetId,
-    writerId,
-    message,
-    createdAt,
-    canDeleteAt: (when) => differenceInSeconds(when, createdAt) <= MAX_MINUTES_FOR_DELETION_IN_SEC,
-  };
-};
+}) => ({
+  tweetId,
+  writerId,
+  message,
+  createdAt,
+  canDeleteAt: (when) => differenceInSeconds(when, createdAt) <= MAX_MINUTES_FOR_DELETION_IN_SEC,
+});
 
 module.exports = Tweet;
