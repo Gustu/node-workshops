8563838c139e214914537982e06ae6b43e2f021c
Bartlomiej Pasik <bartlomiej.pasik@espeo.eu>
2019-11-07 07:45:28 +0100

    Tweet routes only for logged in user. Fixed undefined error.

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const TweetsController = require('./TweetController');
 const AccountController = require('./AccountController');
+const isLoggedIn = require('../middlewares/LoggedIn');
 
 const ApiRoot = ({ tweetConfig }) => {
   const router = Router();
 
-  router.use('/tweets', TweetsController(tweetConfig));
+  router.use('/tweets', isLoggedIn, TweetsController(tweetConfig));
   router.use('/auth', AccountController(tweetConfig));
 
   return router;
--- middlewares/JwtAuthentication.js
+++ middlewares/JwtAuthentication.js
@@ -2,7 +2,7 @@ const Jwt = require('../common/Jwt');
 
 const jwtAuthentication = (req, res, next) => {
   const authHeader = req.header('Authorization');
-  if (authHeader.startsWith('Bearer ')) {
+  if (authHeader && authHeader.startsWith('Bearer ')) {
     const token = authHeader.substring(7, authHeader.length);
     const decoded = Jwt.decode(token);
     req.user = decoded.accountId;
